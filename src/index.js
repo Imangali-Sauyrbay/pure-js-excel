3815772d650103acd3ce3b8b4fd2ece691d70d3a
Imangali <simangali2002@gmail.com>
2021-10-08 20:13:49 +0600

    deleted JSDoc

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import '@styles/main.scss';
 console.log('hello world');
 
-/**
- *Just test
- */
+
 class R {
     static s = 'ssss';
     getStatic = async () => {
       return await Promise.resolve( R.s);
     }
 }
+
 new R().getStatic().then((data) => console.log(data));
 new R().getStatic().then((data) => console.log(data));
